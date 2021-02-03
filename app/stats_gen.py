57d70593699fb5d52cab1841910bc5089e1bb725
jjdelvalle <delvallejacobo@gmail.com>
2021-02-02 18:01:00 -0600

    Prettify graphs and refactor filter code

--- app/stats_gen.py
+++ app/stats_gen.py
@@ -8,6 +8,7 @@ import matplotlib.dates as mdates
 import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
 
+import time
 import datetime
 from typing import Union
 
@@ -20,55 +21,46 @@ def vec_dt_replace(series, year=None, month=None, day=None):
          'month': series.dt.month if month is None else month,
          'day': series.dt.day if day is None else day})
 
+def filter_by(df, by: Union[str, dict], value: Union[str, list] = None, exact: bool = False):
+    if by is None:
+        return df
+
+    filt = [True] * len(df)
+    if isinstance(by, str):
+        by = {by: (value, exact)}
+    for k, v in by.items():
+        value = v[0]
+        exact = v[1]
+        if value is None:
+            continue
+        if isinstance(value, str):
+            if exact:
+                filt = filt & (df[k] == value)
+            else:
+                filt = filt & (df[k].str.contains(value, case=False))
+        elif isinstance(value, list) and len(value) > 0:
+            filt = filt & (df[k].isin(value))
+
+        print(k, value)
+    return df[filt]
+
 @st.cache
 def create_filter(df,
                   degree: str = None,
                   decisionfin: Union[str, list] = None,
                   institution: Union[str, list] = None,
-                  field: Union[str, list] = None,
+                  major: Union[str, list] = None,
                   status: Union[str, list] = None,
-                  seasons: Union[str, list] = None,
-                  gpa: bool = False,
-                  gre: bool = False):
-    filt = [True] * len(df)
+                  season: Union[str, list] = None):
+    filter_dict = {}
+    filter_dict['degree'] = (degree, True)
+    filter_dict['institution'] = (institution, False)
+    filter_dict['decisionfin'] = (decisionfin, True)
+    filter_dict['major'] = (major, True)
+    filter_dict['status'] = (status, True)
+    filter_dict['season'] = (season, True)
 
-    # NEEDS REFACTORING
-    if degree is not None and len(degree) > 0:
-        if isinstance(degree, str):
-            filt = (filt) & (df['degree'].str.contains(degree, case=False))
-        elif isinstance(degree, list):
-            filt = (filt) & (df['degree'].isin(degree))
-    if decisionfin is not None:
-        if isinstance(decisionfin, str):
-            filt = (filt) & (df['decisionfin'].str.contains(decisionfin, case=False))
-        elif isinstance(decisionfin, list):
-            filt = (filt) & (df['decisionfin'].isin(decisionfin))
-    if institution is not None:
-        if isinstance(institution, str):
-            filt = (filt) & (df['institution'].str.contains(institution, case=False))
-        elif isinstance(institution, list):
-            filt = (filt) & (df['institution'].isin(institution))
-    if field is not None and len(field) > 0:
-        if isinstance(field, str):
-            filt = (filt) & (df['major'] == field)
-        elif isinstance(field, list):
-            filt = (filt) & (df['major'].isin(field))
-    if status is not None and len(status) > 0:
-        if isinstance(status, str):
-            filt = (filt) & (df['status'] == status)
-        elif isinstance(status, list):
-            filt = (filt) & (df['status'].isin(status))
-    if seasons is not None and len(seasons) > 0:
-        if isinstance(seasons, str):
-            filt = (filt) & (df['season'] == seasons)
-        elif isinstance(seasons, list):
-            filt = (filt) & (df['season'].isin(seasons))
-    if gpa:
-        filt = (filt) & (~df['gpafin'].isna()) & (df['gpafin'] <= 4)
-    if gre:
-        filt = (filt) & (~df['grev'].isna()) & (~df['grem'].isna()) & (~df['grew'].isna()) & (df['new_gre'])
-    
-    return filt
+    return filter_by(df, filter_dict)
 
 def get_uni_stats(u_df,
 					search: str = None,
@@ -79,7 +71,9 @@ def get_uni_stats(u_df,
 					seasons: list = None,
                     stack: bool = False,
 					hue: str = 'decisionfin',
-                    filt = None):
+                    debug: bool = False):
+    if debug:
+        start_time = time.time()
     title = title if title is not None else search
     if isinstance(degree, str) and degree not in ['MS', 'PhD', 'MEng', 'MFA', 'MBA', 'Other']:
         degree = 'PhD'
@@ -88,19 +82,9 @@ def get_uni_stats(u_df,
         gpa_multiple = 'stack'
     else:
         gpa_multiple = 'dodge'
-    # Clean up the data a bit, this probably needs a lot more work
-    # Maybe its own method, too
-    u_df = u_df.copy()
-    u_df = u_df[~u_df['decdate'].isna()]
-    u_df.loc[:,'year'] = u_df['decdate'].str[-4:].astype(int)
-    u_df = u_df[(u_df['year'] > 2000) & (u_df['year'] < datetime.datetime.now().year)]
-    # Normalize to 2020. 2020 is a good choice because it's recent AND it's a leap year
-    u_df.loc[:, 'uniform_dates'] = vec_dt_replace(pd.to_datetime(u_df['decdate']), year=2020)
-    # Get december dates to be from "2019" so Fall decisions that came in Dec come before the Jan ones.
-    dec_filter = u_df['uniform_dates'] > datetime.datetime.strptime('2020-11-30', '%Y-%m-%d')
-    u_df.loc[dec_filter, 'uniform_dates'] = vec_dt_replace(pd.to_datetime(u_df[dec_filter]['uniform_dates']), year=2019)
 
     # Trying to pick red/green colorblind-friendly colors
+    sns.set_theme(style="white", font_scale=.7)
     flatui = ["#2eff71", "#ff0000", "#0000ff"]
     sns.set_palette(flatui)
     acc_patch = mpatches.Patch(color='#2eff7180')
@@ -114,19 +98,32 @@ def get_uni_stats(u_df,
     if hue == 'status':
         hue_order = ['American', 'International', 'International with US Degree', 'Other']
 
+    if debug:
+        print("prep time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
+
     # This generates 4 graphs, so let's make it a 2x2 grid
     fig, ax = plt.subplots(2,2)
-    fig.set_size_inches(20, 20)
+    fig.set_size_inches(7, 7)
     
     # Timeline stats
-    if filt is None:
-        mscs_filt = create_filter(u_df, degree, ['Accepted', 'Rejected', 'Interview'], search, field, status, seasons)
-    else:
-        mscs_filt = filt
-    mscs_filt = (mscs_filt) & (u_df['uniform_dates'].astype(str) <= '2020-06-00')
-    if sum(mscs_filt) == 0:
+    u_df = create_filter(u_df, degree, ['Accepted', 'Rejected', 'Interview'], search, field, status, seasons)
+    if len(u_df) < 1:
         return fig
-    sns.histplot(data=u_df[mscs_filt],
+
+    if debug:
+        print("filter time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
+
+    graph_filt =  u_df['uniform_dates'] < datetime.datetime(2020, 6, 1)
+    if sum(graph_filt) == 0:
+        return fig
+
+    if debug:
+        print("preptimeline time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
+
+    sns.histplot(data=u_df[graph_filt],
                  x='uniform_dates',
                  hue=hue,
                  cumulative=True,
@@ -135,6 +132,10 @@ def get_uni_stats(u_df,
                  fill=False,
                  hue_order=hue_order,
                  ax=ax[0][0])
+
+    if debug:
+        print("actual timeline plot time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
 
     locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
     formatter = mdates.ConciseDateFormatter(locator)
@@ -164,27 +165,31 @@ def get_uni_stats(u_df,
     # Add frequency counts
     if h is not None and l is not None:
         if hue == 'decisionfin':
-            counts = u_df[mscs_filt][hue].value_counts().reindex(hue_order)
+            counts = u_df[graph_filt][hue].value_counts().reindex(hue_order)
             l = [f'{value} (n={count})' for value, count in counts.iteritems()]
             ax[0][0].legend(handles=[acc_line, rej_line, int_line], labels=l, title="Decision")
 
     ax[0][0].set_xlabel("Date")
     ax[0][0].set_ylabel("Count")
-    ax[0][0].set_title("Cumsum of decisions")
+    ax[0][0].set_title("Decision Timeline", fontsize=10)
+    if debug:
+        print("timeline aesthetics time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
     
     # Get GPA stats
-    gpa_filt = (mscs_filt) & (~u_df['gpafin'].isna()) & (u_df['gpafin'] <= 4)
+    gpa_filt = (~u_df['gpafin'].isna()) & (u_df['gpafin'] <= 4)
+    gpa_bins = 10 if gpa_multiple == 'dodge' else 20
     if sum(gpa_filt) > 0:
         sns.histplot(data=u_df[gpa_filt],
                      x='gpafin',
                      hue=hue,
                      hue_order=hue_order,
                      multiple=gpa_multiple,
-                     bins=20,
+                     bins=gpa_bins,
                      ax=ax[0][1])
         ax[0][1].set_xlabel("GPA")
         ax[0][1].set_ylabel("Count")
-        ax[0][1].set_title("GPA Distribution (stacked values)")
+        ax[0][1].set_title("GPA Distribution ({0} values)".format(gpa_multiple), fontsize=10)
         # Add frequency counts
         h, l = ax[0][1].get_legend_handles_labels()
         if h is not None and l is not None:
@@ -192,9 +197,12 @@ def get_uni_stats(u_df,
                 counts = u_df[gpa_filt][hue].value_counts().reindex(hue_order)
                 l = [f'{value} (n={count})' for value, count in counts.iteritems()]
                 ax[0][1].legend(handles=[acc_patch, rej_patch, int_patch], labels=l, title="Decision")
+    if debug:
+        print("gpa time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
 
     # Get GRE stats
-    gre_filt = (mscs_filt) & (~u_df['grev'].isna()) & (~u_df['grem'].isna()) & (u_df['new_gre'])
+    gre_filt = (~u_df['grev'].isna()) & (~u_df['grem'].isna()) & (u_df['new_gre'])
     if sum(gre_filt) == 0:
         return fig
     dfq = u_df[gre_filt][['grem', hue]]
@@ -210,7 +218,8 @@ def get_uni_stats(u_df,
                 x='gre_type',
                 y='score',
                 hue=hue,
-                linewidth=2.5,
+                linewidth=1,
+                fliersize=1,
                 hue_order=hue_order,
                 ax=ax[1][0])
     leg = ax[1][0].get_legend()
@@ -218,15 +227,19 @@ def get_uni_stats(u_df,
         leg.set_title('Decision')
     ax[1][0].set_xlabel("GRE Section")
     ax[1][0].set_ylabel("Score")
-    ax[1][0].set_title("GRE Score distribution")
+    ax[1][0].set_title("GRE Score distribution", fontsize=10)
+    if debug:
+        print("greqv time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
     
     # Get GRE AWA stats
-    gre_filt = (mscs_filt) & (~u_df['grew'].isna()) & (u_df['new_gre'])
+    gre_filt = (~u_df['grew'].isna()) & (u_df['new_gre'])
     sns.boxplot(data=u_df[gre_filt],
                 x=['AWA'] * len(u_df[gre_filt]),
                 y='grew',
                 hue=hue,
-                linewidth=2.5,
+                linewidth=1,
+                fliersize=1,
                 hue_order=hue_order,
                 ax=ax[1][1])
     leg = ax[1][1].get_legend()
@@ -234,10 +247,15 @@ def get_uni_stats(u_df,
         leg.set_title('Decision')
     ax[1][1].set_xlabel("GRE Section")
     ax[1][1].set_ylabel("Score")
-    ax[1][1].set_title("GRE AWA Score distribution")
+    ax[1][1].set_title("GRE AWA Score distribution", fontsize=10)
+    if debug:
+        print("grew time: %.2f seconds" % (time.time() - start_time))
+        start_time = time.time()
     
-    # Save file to output directory
-    fig.suptitle(title + ', ' + ', '.join(field) + ' ' + ', '.join(degree) + '', size='xx-large')
+
+    sns.despine(left=True)
+    fig.suptitle(f"{title} {', '.join(field)} {', '.join(degree)}", size=20)
+    fig.tight_layout()
     return fig
 
 @st.cache
@@ -248,6 +266,14 @@ def load_data():
     grad_df.loc[:, 'major'] = grad_df['major'].str.strip()
     grad_df.loc[:, 'major'] = grad_df['major'].str.replace(r'[^\w ]\(\)', '', regex=True)
     grad_df = grad_df[(grad_df['new_gre'] == True) | (grad_df['new_gre'].isna())]
+    grad_df = grad_df[~grad_df['decdate'].isna()]
+    grad_df.loc[:,'year'] = grad_df['decdate'].str[-4:].astype(int)
+    grad_df = grad_df[(grad_df['year'] > 2000) & (grad_df['year'] < datetime.datetime.now().year)]
+    # Normalize to 2020. 2020 is a good choice because it's recent AND it's a leap year
+    grad_df.loc[:, 'uniform_dates'] = vec_dt_replace(pd.to_datetime(grad_df['decdate']), year=2020)
+    # Get december dates to be from "2019" so Fall decisions that came in Dec come before the Jan ones.
+    dec_filter = grad_df['uniform_dates'] > datetime.datetime.strptime('2020-11-30', '%Y-%m-%d')
+    grad_df.loc[dec_filter, 'uniform_dates'] = vec_dt_replace(pd.to_datetime(grad_df[dec_filter]['uniform_dates']), year=2019)
     return grad_df
 
 grad_df = load_data()
@@ -259,6 +285,15 @@ seasons = grad_df['season'].drop_duplicates().sort_values().tolist()
 status = grad_df['status'].drop_duplicates().sort_values().tolist()
 
 st.title('GradCafe Stats Generator')
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+.block-container {padding: 0px}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.sidebar.markdown('## Filters')
 
@@ -277,23 +312,6 @@ st.sidebar.markdown('## Display options')
 # Display options
 stack = st.sidebar.checkbox('Stack bars in GPA plot', value=False)
 
-# grad_df.drop(columns=['decdate_ts', 'date_add_ts'], inplace=True)
-# grad_df.columns = ['Institution',
-# 					'Major',
-# 					'Degree',
-# 					'Season',
-# 					'Decision',
-# 					'Notification Method',
-# 					'Decision Date',
-# 					'GPA',
-# 					'GRE Verbal',
-# 					'GRE Quant',
-# 					'GRE AWA',
-# 					'GRE Subject',
-# 					'Status',
-# 					'Entry Date',
-# 					'Comment']
-
 fig = get_uni_stats(grad_df,
 			  search=inst_choice,
 			  title=inst_choice,
@@ -301,6 +319,7 @@ fig = get_uni_stats(grad_df,
 			  field=major_choice,
 			  status=status_choice,
 			  seasons=season_choice,
-              stack=stack)
+              stack=stack,
+              debug=False)
 
 st.pyplot(fig)
